0f4ab912d1c627c05aff24c33cd8cdec777d89f7
LukeCPadmore <82272883+LukeCPadmore@users.noreply.github.com>
2025-12-04 19:34:31 +0000

    day 3 part 2

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from typing import List
 from collections.abc import Generator
 
+def argmax(arr) -> int:
+    return max(range(len(arr)), key=lambda x: arr[x])
 
 def parse_lines(filepath: Path) -> Generator[List[int],None, None]:
     with open(filepath) as f:
@@ -10,19 +12,23 @@ def parse_lines(filepath: Path) -> Generator[List[int],None, None]:
 def part1(banks) -> int:
     def largest_joltage(bank) -> int: 
         l = max(range(len(bank) - 1), key=lambda x: bank[x])
-        r = max(range(l + 1, len(bank)), key=lambda x: bank[x])
+        r = max(range(l,len(bank)), key=lambda x: bank[x])
         return bank[l] * 10 + bank[r]
     return sum([largest_joltage(bank) for bank in banks])
 
 def part2(banks) -> int:
     def largest_joltage(bank) -> int: 
-        d = {i:bank[i] for i in range(len(bank))}
-        indices_to_remove = sorted(range(len(bank)), key = lambda x:(bank[x],x))[:-12]
-        return int("".join([str(bank[i]) for i in range(len(bank)) if i not in indices_to_remove]))
-    print([largest_joltage(bank) for bank in banks])
+        chosen_indices = []
+        p = 0 
+        for i in range(0,12):
+            limit = len(bank) - (12 - i)
+            best_index = argmax(bank[p:limit + 1])
+            chosen_indices.append(str(bank[p + best_index]))
+            p += best_index + 1
+        return int("".join(chosen_indices))
     return sum(largest_joltage(bank) for bank in banks)
 
 if __name__ == "__main__":
-    banks = parse_lines('test.txt')
+    banks = parse_lines('input.txt')
     print(part1(banks))
     print(part2(banks))
